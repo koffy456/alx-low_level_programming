76e15007eb08224236905a82d107252c4f486d8a
koffy456 <kofinyame830@gmail.com>
2022-03-17 15:12:05 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+void more_numbers(void)
+{
+	int i;
+	int h;
+	for (h = 0; h <= 9; h++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+
+
+	        	if (i > 9)
+	        	{ 
+			    _putchar(i / 10 + '0');
+	         	}
+		       _putchar(i % 10 + '0');
+	        }
+	        _putchar('\n');
+	}
+}
