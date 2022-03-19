830f1ac7dc576ad284f21bcdf3759981115d467a
koffy456 <kofinyame830@gmail.com>
2022-03-19 01:45:01 +0000

    print fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,25 +7,26 @@
  */
 int main(void)
 {
-	int n;
+        int n;
 
-	for (n = 1; n <= 100; n++)
-	{
-		if (n % 3 == 0) && (n % 5 == 0))
-		        printf("FizzBuzz");
-		else if (n % 5 == 0)
-		        printf("Buzz");
-			
-		else if (n % 3 == 0)
-	                printf("Buzz");
-			
-		else    
-		        printf("%i", n);
-		
-		if (n < 100)
-			printf(" ");
-		else printf("\n");
-	}
-	
-	return (0);
+        for (n = 1; n <= 100; n++)
+        {
+                if (n % 3 == 0) && (n % 5 == 0))
+                        printf("FizzBuzz");
+                else if (n % 5 == 0)
+                        printf("Buzz");
+
+                else if (n % 3 == 0)
+                        printf("Buzz");
+
+                else
+                        printf("%i", n);
+
+                if (n < 100)
+                        printf(" ");
+                else printf("\n");
+        }
+
+        return (0);
 }
+
