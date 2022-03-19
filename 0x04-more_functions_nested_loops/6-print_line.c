0856ec0180e4b8f6870300677d95dabf15e9ef8e
koffy456 <kofinyame830@gmail.com>
2022-03-19 00:45:08 +0000

    print shortest distance between lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ *
+ * @n: integer to print stuff?
+ */
+void print_line(int n)
+{
+	int num_lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+                for (num_lines = 1; num_lines <= n; numlines++)
+	        {
+		        _putchar('_');
+	        }
+	_putchar('\n');
+}
