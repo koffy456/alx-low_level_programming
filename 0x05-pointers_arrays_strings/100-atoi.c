9c56e22ca0227544f18a284e0373bd6500e4aa58
koffy456 <kofinyame830@gmail.com>
2022-03-21 18:16:24 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,77 +1,31 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * Function to randomly generates password of length N
- */
+#include "main.h"
 
-void randomPasswordGeneration(int N)
+/**
+ * _atoi - print reverse characters.
+ * @s: Array
+ *
+ * Return: Always 0.
+ */
+int _atoi(char *s)
 {
-    // Initialize counter
-    int i = 0;
-  
-    int randomizer = 0;
-  
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-    srand((unsigned int)(time(NULL)));
-  
-    // Array of numbers
-    char numbers[] = "0123456789";
-  
-    // Array of small alphabets
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-  
-    // Array of capital alphabets
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-  
-    // Array of all the special symbols
-    char symbols[] = "!@#$^&*?";
-  
-    // Stores the random password
-    char password[N];
-  
-    // To select the randomizer
-    // inside the loop
-    randomizer = rand() % 4;
-  
-    // Iterate over the range [0, N]
-    for (i = 0; i < N; i++) {
-  
-        if (randomizer == 1) {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 2) {
-            password[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
-}
-  
-// Driver Code
-int main()
-{
-    // Length of the password to
-    // be generated
-    int N = 10;
-  
-    // Function Call
-    randomPasswordGeneration(N);
-  
-    return 0;
+	int i = 0, sign = 1;
+	unsigned int num = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] < '0' || s[i] > '9') && (s[i] == '-'))
+		{
+			sign = sign * -1;
+		}
+		else if ((s[i] >= '0' && s[i] <= '9'))
+		{
+			num = num * 10 + (s[i] - '0');
+			if ((s[i + 1] < '0' || s[i + 1] > '9'))
+			{
+				break;
+			}
+		}
+		i++;
+	}
+	return (num * sign);
 }
