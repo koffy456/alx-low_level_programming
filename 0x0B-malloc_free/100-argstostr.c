dfdf7dbc16025adaf8facf263507c0e1554b7ea4
koffy456 <kofinyame830@gmail.com>
2022-04-07 02:51:51 +0000

    args

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,21 +1,45 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * free_grid - frees the memory allocate for the grid
- * created by alloc_grid()
- * @grid: grid to free
- * @height: height of the grid
+ * argstostr - Concatenates all arguments of the program into a string;
+ *             arguments are separated by a new line in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
+ *
+ * Return: If ac == 0, av == NULL, or the function fails - NULL.
+ *         Otherwise - a pointer to the new string.
  */
-void free_grid(int **grid, int height)
+char *argstostr(int ac, char **av)
 {
-	int i;
+	char *str;
+	int arg, byte, index, size = ac;
 
-	if (grid == NULL || height == 0)
-		return;
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-	for (i = 0; i < height; i++)
-		free(grid[i]);
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
 
-	free(grid);
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
 }
