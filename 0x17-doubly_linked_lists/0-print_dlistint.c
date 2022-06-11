e4f0b72ece29cc103b8edfda8b75177e8839ad2a
koffy456 <kofinyame830@gmail.com>
2022-06-11 04:56:56 +0000

    0print

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
