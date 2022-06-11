6099630ec9985b0d2d9774f522fd5f878c4fe26d
koffy456 <kofinyame830@gmail.com>
2022-06-11 05:04:13 +0000

    1dlist

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
